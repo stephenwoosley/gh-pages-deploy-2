6a0cf4b13c0e54768ef38885e3c60f32609ef9a7
Stephen Woosley <stephenrwoosley@gmail.com>
2020-08-22 13:13:53 -0400

    edit gitignore

---
+++ build/precache-manifest.cbcfb06f36dfb5b79186fc39b32733ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59573c75acb1d65907e63b6edd4918bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7864a7f6ad74820f3882",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "93de38fef54c9354f0c7",
+    "url": "/static/js/2.74f3d4a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.74f3d4a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7864a7f6ad74820f3882",
+    "url": "/static/js/main.972cee29.chunk.js"
+  },
+  {
+    "revision": "3e33601a9a7d82bbf38b",
+    "url": "/static/js/runtime-main.42eb3da1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
